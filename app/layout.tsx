7207f0a6f49fa97731202a46d815a21e4d5a6290
Steka <stephenkaruru05@gmail.com>
2025-06-29 02:45:13 +0300

    authentication🔐🔐

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,12 @@ import "./globals.css";
 import { Sidebar } from "@/components/sidebar";
 import { Header } from "@/components/header";
 import { usePathname } from "next/navigation";
+import { Inter } from 'next/font/google'
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter', // Optional: for CSS variable support
+})
 export default function RootLayout({
   children,
 }: {
@@ -17,7 +22,7 @@ export default function RootLayout({
   const isAuthPage = pathname.startsWith("/auth");
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
       <head>
         <title>LogitrustKenya</title>
         <meta name="description" content="Trusted logistics platform for Kenyan SMEs" />
